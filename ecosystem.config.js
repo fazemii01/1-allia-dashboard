9d5ef51f9f948323910fbc0dc4039d42ef71eb9a
fazemii01 <bestvatp@gmail.com>
2026-07-17 12:32:47 +0700

    nah id win

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,1 @@
-module.exports = {
-  apps: [
-    {
-      name: 'alliakids-dashboard',
-      script: 'npm',
-      args: 'run preview',
-      shell: true,
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production'
-      }
-    }
-  ]
-};
+// Replaced by ecosystem.config.cjs
